62c61ae3d565b03d246b852881087996f847b9b1
Peter Boisvert <pboisver@umich.edu>
2020-05-28 19:54:23 -0400

    Initial commit

---
+++ src/main/java/com/agylyze/gaction/GactionApplication.java
@@ -0,0 +1,13 @@
+package com.agylyze.gaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GactionApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GactionApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/agylyze/gaction/GactionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agylyze.gaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GactionApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
